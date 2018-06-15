d57069328df1cee9d7695b5988087e5fa476302e
NazeerH <nsHolmes@localadminsmbp.attlocal.net>
2018-06-15 15:00:25 -0500

    added quiz

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,1 +1,20 @@
-/* Quiz.js */
+import React, { Component } from 'react';
+
+let quizData = require('./quiz_data.json');
+
+class Quiz extends Component {
+    constructor(props){
+        super(props);
+        this.state = { quiz_position: 1};
+    }
+    render(){
+        return (
+            <div>
+                <div class="QuizQuestion">{quizData.quiz_questions[0].instructional_text}</div>
+            </div>
+        )
+    }
+}
+
+
+export default Quiz
