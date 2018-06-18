19058f4a461446e0f9e8735aa695385b7cca4f74
NazeerH <nsHolmes@localadminsmbp.attlocal.net>
2018-06-17 22:06:17 -0500

    updated QuizQuestion div

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,7 +10,7 @@ class Quiz extends Component {
     render(){
         return (
             <div>
-                <div class="QuizQuestion">{quizData.quiz_questions[0].instructional_text}</div>
+                <div class="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
             </div>
         )
     }
